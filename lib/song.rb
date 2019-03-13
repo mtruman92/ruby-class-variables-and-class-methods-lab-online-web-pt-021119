61bb3fab1176865e1ea7227120f938df27f375a7
Mickae Truman <mtruman@nyit.edu>
2019-03-13 13:42:02 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -38,4 +38,15 @@ genre_count = {}
   genre_count
 end
 
+def self.artist_count
+  artist_count = {}
+  @@artists.each do |artist|
+    if artist_count.has_key?(artist)
+      artist_count[artist] += 1 
+    else
+      artist_count[artist] = 1
  end 
+ artist_count
+ end
+ 
+ end
